b952ff169149bea632a07145f4e984aec96ce028
Joshua Moon <joshua@moon.uk.net>
2022-07-23 00:54:26 +0100

    Move Reno project from Tseng solution

---
+++ Shojy.FF7.Reno/FF7InteractionService.cs
@@ -0,0 +1,77 @@
+﻿using System.Diagnostics;
+using Shojy.FF7.Reno.Extensions;
+using Shojy.FF7.Reno.MemoryAddresses;
+using Shojy.FF7.Reno.Models;
+
+namespace Shojy.FF7.Reno;
+
+[PublicAPI]
+public interface IFF7InteractionService
+{
+    Task<Process> ConnectToGame();
+    bool GetData(out SaveMap saveMap, out BattleMap battleMap);
+}
+
+// ReSharper disable once InconsistentNaming
+public class FF7InteractionService : IFF7InteractionService
+{
+    private readonly IProcessAccessor _processAccessor;
+    private readonly IMemoryAccessor _memoryAccessor;
+    
+
+    public FF7InteractionService(IProcessAccessor processAccessor, IMemoryAccessor memoryAccessor)
+    {
+        _processAccessor = processAccessor;
+        _memoryAccessor = memoryAccessor;
+    }
+
+    public async Task<Process> ConnectToGame()
+    {
+        var ff7 = await _processAccessor.ScanForProcess("ff7", "ff7_en");
+        _memoryAccessor.SetProcess(ff7);
+        return ff7;
+    }
+
+    public bool GetData(out SaveMap saveMap, out BattleMap battleMap)
+    {
+        var state = true;
+
+        // Default these here to avoid a compile error for "not assigning them" before exiting on a fail state, even
+        // though logically, there is no path that cannot assign them.
+        saveMap = default;
+        battleMap = default;
+
+        try
+        {
+            if (_memoryAccessor.ReadMemory(MemoryLocations.SaveMap, out var saveBytes))
+            {
+                saveMap = saveBytes.ToType<SaveMap>();
+            }
+            else
+            {
+                state = false;
+            }
+
+            if (_memoryAccessor.ReadMemory(MemoryLocations.BattleMap, out var battleBytes))
+            {
+                battleMap = battleBytes.ToType<BattleMap>();
+            }
+            else
+            {
+                state = false;
+            }
+        }
+        catch
+        {
+            state = false;
+        }
+
+        if (!state)
+        {
+            saveMap = default;
+            battleMap = default;
+        }
+
+        return state;
+    }
+}
---
+++ Shojy.FF7.Reno/Models/WindowColor.cs
@@ -0,0 +1,49 @@
+﻿using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace Shojy.FF7.Reno.Models;
+
+[PublicAPI]
+[StructLayout(LayoutKind.Explicit, Size = 0xC)]
+public record struct WindowColor
+{
+    [FieldOffset(0x0)]
+    public FFColor TopLeft;
+
+    [FieldOffset(0x3)]
+    public FFColor TopRight;
+
+    [FieldOffset(0x6)]
+    public FFColor BottomLeft;
+
+    [FieldOffset(0x9)]
+    public FFColor BottomRight;
+
+    public Color Average => GetAverageColor();
+
+
+    /// <summary>
+    /// Calculates the average color from the 4 positional colors. 
+    /// </summary>
+    /// <returns></returns>
+    private Color GetAverageColor()
+    {
+        return Color.FromArgb(
+            red: GetValueAverage(TopLeft.Red, TopRight.Red, BottomLeft.Red, BottomRight.Red),
+            green: GetValueAverage(TopLeft.Green, TopRight.Green, BottomLeft.Green, BottomRight.Green),
+            blue: GetValueAverage(TopLeft.Blue, TopRight.Blue, BottomLeft.Blue, BottomRight.Blue)
+        );
+    }
+
+    /// <summary>
+    /// Uses a squared mean to calculate the average color
+    /// </summary>
+    /// <param name="values"></param>
+    /// <returns></returns>
+    private static int GetValueAverage(params int[] values)
+    {
+        double sum = values.Sum(v => v * v);
+        var mean = Math.Sqrt(sum / values.Length);
+        return (int)Math.Round(mean);
+    }
+}
